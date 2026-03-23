08e2014296381e4ca1857d6c96181520dd776f1f
Bhavishya Gupta <bhavishyagupta001@gmail.com>
2026-03-23 20:45:14 +0530

    user and post backend improvised

--- server/models/postModels.js
+++ server/models/postModels.js
@@ -2,61 +2,18 @@ const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    userName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    text: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String, required: true, trim: true },
   },
   { timestamps: true, _id: true }
 );
 
 const postSchema = new mongoose.Schema(
   {
-    imgPath: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    desc: {
-      type: String,
-      default: "",
-    },
-    format: {
-      type: String,
-      required: true,
-    },
-    mimeType: {
-      type: String,
-      default: "",
-    },
-    likes: {
-      type: Number,
-      default: 0,
-    },
-    liked: {
-      type: Boolean,
-      default: false,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    likedUser: [{ type: String, default: "" }],
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    imageUrl: { type: String, required: true },
+    caption: { type: String, default: "" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     comments: [commentSchema],
   },
   { timestamps: true }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,24 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
-    displayName: { type: String, default: "", trim: true },
-    bio: { type: String, default: "", trim: true },
-    website: { type: String, default: "", trim: true },
-    username: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-      lowercase: true,
-    },
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     password: { type: String, required: true },
-    img: { type: String, default: "" },
-    followings: { type: Number, default: 0 },
-    followers: { type: Number, default: 0 },
-    followersList: [{ type: String, default: "" }],
-    followingsList: [{ type: String, default: "" }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    profilePicture: { type: String, default: "" },
   },
   { timestamps: true }
 );
