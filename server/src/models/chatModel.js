9abddfe2611fcbec35efb2719b374294ba481ddf
Bhavishya Gupta <bhavishyagupta001@gmail.com>
2026-06-20 20:02:32 +0530

    feat: update chat model schema to include group chat fields and message references

--- server/src/models/chatModel.js
+++ server/src/models/chatModel.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema(
   {
+    isGroupChat: { type: Boolean, default: false },
+    chatName: { type: String, trim: true },
+    chatImage: { type: String, default: "" },
     participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }],
+    admins: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+    pinnedMessages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }]
   },
   { timestamps: true }
 );
