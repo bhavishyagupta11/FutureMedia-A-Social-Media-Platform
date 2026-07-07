d5dc7b3380c6a344ee24dc57981663f37edcb8c6
Bhavishya Gupta <bhavishyagupta001@gmail.com>
2026-07-07 22:49:55 +0530

    feat: refactor Home component to include Stories, PostShare, and Posts

--- social/src/pages/Auth/Home/Home.jsx
+++ social/src/pages/Auth/Home/Home.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect } from 'react'
-import PostSide from "../../../components/PostSide/PostSide";
-import ProfileSide from "../../../components/profileSide/ProfileSide";
-import RightSide from "../../../components/RightSide/RightSide";
-import './Home.css';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Stories from "../../../components/Stories/Stories";
+import PostShare from "../../../components/PostShare/PostShare";
+import Posts from "../../../components/Posts/Posts";
+import './Home.css';
+
 const Home = () => {
-const navigate = useNavigate()
-useEffect(() => {
-if (!localStorage.getItem("userId")) {
-navigate("/")
-}
-}, [navigate])
-return (
-<div className="Home">
-<ProfileSide />
-<PostSide />
-<RightSide />
-</div>
-)
-}
-export default Home
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!localStorage.getItem("userId")) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <div className="Home">
+      <div className="home-content">
+        <Stories />
+        <PostShare />
+        <Posts />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
