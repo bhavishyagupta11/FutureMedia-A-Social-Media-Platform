479660c1f8113c74bcbb92e9e8c799bbb2ac8bc5
Bhavishya Gupta <bhavishyagupta001@gmail.com>
2026-08-04 07:50:32 +0530

    feat: Refactor role middleware to use error handling instead of throwing errors directly

--- server/src/middleware/role.js
+++ server/src/middleware/role.js
@@ -1,8 +1,9 @@
 const roleMiddleware = (...roles) => {
   return (req, res, next) => {
     if (!req.user || !roles.includes(req.user.role)) {
-      res.status(403);
-      throw new Error(`User role '${req.user ? req.user.role : "undefined"}' is not authorized to access this route`);
+      const err = new Error(`User role '${req.user ? req.user.role : "undefined"}' is not authorized to access this route`);
+      err.status = 403;
+      return next(err);
     }
     next();
   };
